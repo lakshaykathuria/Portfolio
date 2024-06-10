ebef370f3ebf99c7e92f0d22c586c65d9e6f7219
Lakshay Kathuria <74535080+lakshaykathuria@users.noreply.github.com>
2024-06-10 22:18:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,94 @@
+const socialsEl = document.querySelector("#socials");
+const projectsEl = document.querySelector("#projects");
+const writingEl = document.querySelector("#wrtiting");
+
+const socialHandles = [
+  {
+    Link: "https://x.com/Lakshay_1808",
+    platform: "twitter",
+  },
+  {
+    Link: "https://github.com/lakshaykathuria",
+    platform: "github",
+  },
+  {
+    Link: "https://www.instagram.com/lakshay_kathuria_/",
+    platform: "Instagram",
+  },
+  {
+    Link: "https://www.linkedin.com/in/kathuria-lakshay/",
+    platform: "linkedin",
+  },  
+];
+
+
+const projectsArray = [
+  {
+    title: "J.A.R.V.I.S",
+    desc: " The J.A.R.V.I.S is a Python virtual assistant project inspired by Tony Stark's intelligent AI system from the Marvel universe. It is designed to provide a range of functionalities and assist users with tasks using natural language processing and automation capabilities. While this project is a simplified version of the fictional J.A.R.V.I.S, it serves as a foundation for building a voice-controlled assistant.",
+    githubLink: "https://github.com/lakshaykathuria/JARVIS",
+    // demoLink: " ",
+  },
+  {
+    title: "Bank Management System",
+    desc: "A Bank Management System project in Python and MySQL is a software application that allows users to perform banking transactions such as creating and managing accounts, depositing and withdrawing funds, checking balances, and viewing transaction history.",
+    githubLink: "https://github.com/lakshaykathuria/Bank-management-system",
+    // demoLink: " ",
+  },
+  {
+    title: "Flipkart Clone",
+    desc: "The Flipkart Clone project is a web-based e-commerce application that emulates the appearance and basic functionality of the popular online shopping platform, Flipkart. This project is primarily built using HTML and CSS, focusing on replicating the user interface (UI) and layout of the Flipkart website.",
+    githubLink: "https://github.com/lakshaykathuria/Flipkart-Clone",
+    // demoLink: " ",
+  },
+];
+
+
+
+socialHandles.forEach((social, index) => {
+  const a = document.createElement("a");
+  a.href = social.Link;
+  a.textContent = social.platform;
+  a.target = "_blank";
+
+  socialsEl.appendChild(a);
+});
+
+projectsArray.forEach((project, index) => {
+  const div = document.createElement("div");
+  div.style.display = "flex"
+  div.style.flexDirection = "column"
+  div.style.gap = "10px"
+  div.style.marginLeft = "15px"
+  div.style.color = "rgb(229, 231, 235)"
+  const h3 = document.createElement("h3");
+  h3.className = "project-title";
+  h3.textContent = `- ${project.title}`;
+  h3.style.marginLeft = "-15px"
+  const p = document.createElement("p");
+  p.textContent = project.desc;
+  const links = document.createElement("div")
+  links.className = "links"
+  const githubLink = document.createElement("a");
+  githubLink.target = "_blank";
+  githubLink.href = project.githubLink;
+  githubLink.textContent = "github";
+  githubLink.style.display = project.githubLink ? "block" : "none"
+  const demoLink = document.createElement("a");
+  demoLink.target = "_blank";
+  demoLink.href = project.demoLink;
+  demoLink.textContent = "demo";
+
+  links.append(githubLink, demoLink);
+
+  div.append(h3, p, links);
+
+  projectsEl.appendChild(div);
+
+});
+
+
+
+
+
+
